117152b3867a30861d0d6fb38402ebc746cf2bc6
soumyabishi <soumyabishi@outlook.com>
2020-05-27 17:16:22 +0530

    Login page slider added

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="Login">
     <div class="tw-flex tw-justify-center tw-items-stretch tw-h-screen">
-      <div class="left-content tw-bg-primary tw-justify-center tw-items-center tw-flex tw-flex-col">
-        <div class="tw-font-medium tw-font-rubik tw-text-28px tw-mb-4px tw-text-white">Case Study<br>
-          accenture</div>
+      <div class="left-content tw-justify-center tw-items-center tw-flex tw-flex-col">
+        <VueSlickCarousel :arrows="true" :dots="true" :autoplay="true" class="login_slider">
+          <div class="slider slider_1">
+              <div class="text">Our Benchmarks are British Psychology Approved and highly accurate showing a knee-jerk of your unique strengths and gap areas.</div>
+          </div>
+
+          <div class="slider slider_2">
+            <div class="text">Our benchmarks can be taken right off you phones are are just 10-15 minutes.</div>
+          </div>
+
+          <div class="slider slider_3">
+            <div class="text">Our benchmarks pinpoints behavioral job fitment and the skills needed to be developed for the role.
+            </div>
+          </div>
+
+        </VueSlickCarousel>
       </div>
 
       <div class="right-content tw-flex tw-flex-col tw-relative">
 
         <a><img src="/src/assets/images/gleac-logo-colored.png" class="logo tw-absolute tw-top-30px tw-left-30px"></a>
         <div class="tw-font-rubik tw-text-16px tw-absolute tw-top-30px tw-right-30px">Don’t have an account? <a href="/" class="tw-text-primary tw-underline hover:tw-underline">Sign Up</a></div>
-        <h1 class="tw-text-32px tw-text-text-color tw-leading-38px tw-mb-6px tw-font-bold">Human Skills Matter</h1>
+        <h1 class="tw-text-32px tw-text-text-color tw-leading-38px tw-mb-12px tw-font-bold">Human Skills Matter</h1>
         <p class="tw-text-16px tw-text-secondary tw-font-medium tw-leading-26px tw-mb-34px">Gleac measures and
           develops human skills for any job in 10 minutes a day using A.I.</p>
 
@@ -66,6 +79,7 @@ export default {
   .Login{
     .left-content{
       width: 40%;
+      background-color: #1C1C1C;
     }
     .right-content{
       width: 60%;
@@ -75,6 +89,54 @@ export default {
     .logo{
       width: 111px;
     }
+    .login_slider{
+      width: 100%;
+      position: relative;
+      div:focus{
+        outline: none;
+      }
+
+      .slider{
+        position: relative;
+      }
+      .slider_1{
+        background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0, 1) 100%), url("/src/assets/images/login_slider/slider_1.jpg") no-repeat center center;
+        background-size: cover;
+        height: 100vh;
+      }
+
+      .slider_2{
+        background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0, 1) 100%), url("/src/assets/images/login_slider/slider_2.jpg") no-repeat center center;
+        background-size: cover;
+        height: 100vh;
+      }
+      .slider_3{
+        background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(0,0,0, 1) 100%), url("/src/assets/images/login_slider/slider_3.jpg") no-repeat center center;
+        background-size: cover;
+        height: 100vh;
+      }
+      .text{
+        position: absolute;
+        bottom: 100px;
+        color: #fff;
+        font-family: Rubik, sans-serif;
+        padding: 0 20px;
+        font-size: 16px;
+        line-height: 1.6;
+      }
+
+      .slick-dots{
+        bottom: 55px !important;
+      }
+      .slick-dots li.slick-active button:before{
+        color:white !important;
+      }
+
+      .slick-dots li button:before{
+        color:white !important;
+      }
+    }
   }
 
 </style>
+
