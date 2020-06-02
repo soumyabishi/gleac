b3c6f97a284c40341278c224f5bf5902e2bf91ba
soumyabishi <soumyabishi@outlook.com>
2020-06-02 13:11:27 +0530

    Vue router # removed from url and scroll position fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import store from './store'
 
 import AppNav from './components/common/AppNav'
@@ -10,6 +9,15 @@ Vue.component("app-nav", AppNav)
 
 import AppFooter from './components/common/AppFooter'
 Vue.component("app-footer", AppFooter)
+
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
+import BenchmarkLogin from '@/components/BenchmarkLogin'
+import BenchmarkSignup from '@/components/BenchmarkSignup'
+import Terms from '@/components/Terms'
+import DataPrivacy from '@/components/DataPrivacy'
+import CookiePolicy from '@/components/CookiePolicy'
 
 var VueScrollTo = require('vue-scrollto');
 Vue.use(VueScrollTo, {
@@ -39,6 +47,65 @@ Vue.use(require('vue-cookies'))
 import '../src/assets/css/tailwind.css';
 
 Vue.config.productionTip = false
+
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (from && to.name === from.name && JSON.stringify(to.params) === JSON.stringify(from.params)) {
+      return;
+    }
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/benchmark-login',
+      name: 'BenchmarkLogin',
+      component: BenchmarkLogin
+    },
+    {
+      path: '/benchmark-signup',
+      name: 'BenchmarkSignup',
+      component: BenchmarkSignup
+    },
+    {
+      path: '/terms',
+      name: 'Terms',
+      component: Terms
+    },
+    {
+      path: '/data-privacy',
+      name: 'DataPrivacy',
+      component: DataPrivacy
+    },
+    {
+      path: '/cookie-privacy',
+      name: 'CookiePolicy',
+      component: CookiePolicy
+    }
+  ]
+})
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,57 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
-import Login from '@/components/Login'
-import Signup from '@/components/Signup'
-import BenchmarkLogin from '@/components/BenchmarkLogin'
-import BenchmarkSignup from '@/components/BenchmarkSignup'
-import Terms from '@/components/Terms'
-import DataPrivacy from '@/components/DataPrivacy'
-import CookiePolicy from '@/components/CookiePolicy'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    },
-    {
-      path: '/signup',
-      name: 'Signup',
-      component: Signup
-    },
-    {
-      path: '/benchmark-login',
-      name: 'BenchmarkLogin',
-      component: BenchmarkLogin
-    },
-    {
-      path: '/benchmark-signup',
-      name: 'BenchmarkSignup',
-      component: BenchmarkSignup
-    },
-    {
-      path: '/terms',
-      name: 'Terms',
-      component: Terms
-    },
-    {
-      path: '/data-privacy',
-      name: 'DataPrivacy',
-      component: DataPrivacy
-    },
-    {
-      path: '/cookie-privacy',
-      name: 'CookiePolicy',
-      component: CookiePolicy
-    }
+
   ]
 })
